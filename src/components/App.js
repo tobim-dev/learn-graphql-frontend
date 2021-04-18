5984b6de68c8813620df3b50b21e3208fade2721
tobim-dev <mail@tmeyer.me>
2021-04-18 17:44:31 +0200

    add search functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import LinkList from "./LinkList";
 import { Switch, Route } from "react-router-dom";
 import Login from "./Login";
+import Search from "./Search";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </div>
     </div>
